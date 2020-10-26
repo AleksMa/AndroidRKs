2dace8a6cba059a5fc344e81544b8bccc9dc1fec
Alexey Mamaev <a.mamaev@corp.mail.ru>
2020-10-26 12:08:58 +0300

    [+] basic menu, settings & validation

--- RK1/app/src/main/java/com/bmstu/iu9/swimrunners/androidrk1/MainActivity.kt
+++ RK1/app/src/main/java/com/bmstu/iu9/swimrunners/androidrk1/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.bmstu.iu9.swimrunners.androidrk1
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val itemID: Int = item.itemId
+        if (itemID == R.id.settings_menu_item) {
+            startActivity(Intent(this, SettingsActivity::class.java))
+        } else if (itemID == R.id.refresh_menu_item) {
+            Toast.makeText(this, "FIXME!", Toast.LENGTH_SHORT).show()
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
---
+++ RK1/app/src/main/java/com/bmstu/iu9/swimrunners/androidrk1/SettingsActivity.kt
@@ -0,0 +1,11 @@
+package com.bmstu.iu9.swimrunners.androidrk1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
