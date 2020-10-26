c5fdb41c9be4e7049b933b7ec50c303559feb909
Alexey Mamaev <a.mamaev@corp.mail.ru>
2020-10-26 12:41:14 +0300

    [*] packages & res/strings refactoring

--- RK1/app/src/main/java/com/bmstu/iu9/swimrunners/androidrk1/activities/SettingsActivity.kt
+++ RK1/app/src/main/java/com/bmstu/iu9/swimrunners/androidrk1/activities/SettingsActivity.kt
@@ -1,7 +1,8 @@
-package com.bmstu.iu9.swimrunners.androidrk1
+package com.bmstu.iu9.swimrunners.androidrk1.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bmstu.iu9.swimrunners.androidrk1.R
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- RK1/app/src/main/java/com/bmstu/iu9/swimrunners/androidrk1/fragments/ExtraInfoFragment.kt
+++ RK1/app/src/main/java/com/bmstu/iu9/swimrunners/androidrk1/fragments/ExtraInfoFragment.kt
@@ -1,10 +1,11 @@
-package com.bmstu.iu9.swimrunners.androidrk1
+package com.bmstu.iu9.swimrunners.androidrk1.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bmstu.iu9.swimrunners.androidrk1.R
 
 
 class ExtraInfoFragment : Fragment() {
