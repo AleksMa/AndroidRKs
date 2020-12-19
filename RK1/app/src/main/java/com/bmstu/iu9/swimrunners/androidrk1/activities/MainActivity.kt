a740a5f08eb4ad18a5756e75e243f3a00b76eb3b
Alexey Mamaev <a.mamaev@corp.mail.ru>
2020-12-19 14:47:56 +0300

    added floating action bar

--- RK1/app/src/main/java/com/bmstu/iu9/swimrunners/androidrk1/activities/MainActivity.kt
+++ RK1/app/src/main/java/com/bmstu/iu9/swimrunners/androidrk1/activities/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -14,11 +15,16 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import com.bmstu.iu9.swimrunners.androidrk1.R
 import com.bmstu.iu9.swimrunners.androidrk1.fragments.ListFragment
 import com.bmstu.iu9.swimrunners.androidrk1.viewModels.RestCoinViewModel
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
 
     private val vm: RestCoinViewModel by viewModels()
+
+    private lateinit var fab: FloatingActionButton
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +35,11 @@ class MainActivity : AppCompatActivity() {
         val navController = navHostFragment.navController
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
+
+        fab = findViewById(R.id.fab)
+        fab.setOnClickListener {
+            startActivity(Intent(this, SettingsActivity::class.java))
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -45,9 +56,7 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val itemID: Int = item.itemId
 
-        if (itemID == R.id.settings_menu_item) {
-            startActivity(Intent(this, SettingsActivity::class.java))
-        } else if (itemID == R.id.refresh_menu_item) {
+        if (itemID == R.id.refresh_menu_item) {
             vm.loadTimeseries(ListFragment.currentCryptoType)
         }
 
